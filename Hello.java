c42c4f2d3beb51fad0830bcc81f27a5191a4e18b
vjosey <53488086+vjosey@users.noreply.github.com>
2019-11-08 10:18:06 -0500

    Change main method

--- Hello.java
+++ Hello.java
@@ -1,6 +1,14 @@
 class Hello{
 
+	static int count;
 
+	public static void main(String[] agrs){
+	Scanner sc = new Scanner(System.in);
+
+	int n = sc.nextInt();
+ 	System.out.printf("%03d",n);
+
+	}
 
 
 }
